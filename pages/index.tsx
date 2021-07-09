b45b466e5841d56ee47e33071b1de432f0bc6d1c
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-07-09 16:54:30 +0800

    Add news and reviews slider, and upcoming releases component to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
-const Home = () => (
-  <div>
-    Home
-  </div>
+import { FC } from 'react';
+import { Box } from '@chakra-ui/react';
+
+import { Layout } from 'components/core';
+import { NewsCarousel } from 'components/news';
+import { ReviewsCarousel } from 'components/review';
+import { UpcomingReleases } from 'components/game';
+
+const HomePage: FC = () => (
+  <>
+    <Layout first pb='125px'>
+      <Box w='full'>
+        <Box d='inline-block' w='825px' h='467px'>
+          <NewsCarousel />
+        </Box>
+        <Box d='inline-block' w='825px' h='467px'>
+          <ReviewsCarousel />
+        </Box>
+      </Box>
+    </Layout>
+    <UpcomingReleases />
+  </>
 );
 
-export default Home;
+export default HomePage;
