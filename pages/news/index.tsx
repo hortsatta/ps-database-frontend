5a15d1683e3b0fb296bbdc5e4d786376d117c00a
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-07-09 16:39:49 +0800

    Add placeholder for pages

---
+++ pages/news/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const NewsPage: FC = () => (
+  <h2>Gaming New</h2>
+);
+
+export default NewsPage;
---
+++ pages/reviews.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const ReviewsPage: FC = () => (
+  <h2>Gaming Reviews</h2>
+);
+
+export default ReviewsPage;
