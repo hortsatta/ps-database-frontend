51d210c7f788a6307282f0cc5e1bda6a4272dfe8
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-07-10 09:01:10 +0800

    Add game model

---
+++ models/game.model.ts
@@ -0,0 +1,15 @@
+import { Review } from './review.model';
+
+export type Game = {
+  slug: string;
+  title: string;
+  description: string;
+  // developer: string;
+  // publisher: string;
+  // releaseDate: Date;
+  // coverImage: string;
+  // featureImage: string;
+  // platforms: string[];
+  // userId: number;
+  // reviews: Review[];
+}
--- models/index.ts
+++ models/index.ts
@@ -1,3 +1,4 @@
 export * from './common.model';
+export * from './game.model';
 export * from './news.model';
 export * from './review.model';
